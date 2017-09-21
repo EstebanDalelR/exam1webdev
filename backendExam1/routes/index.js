329826608caf1b323c43caa7fb036334cb9932c3
estebandalelr <estebandalelr@gmail.com>
2017-09-21 14:50:21 -0500

    Got the API call

--- backendExam1/routes/index.js
+++ backendExam1/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var GitHubApi = require("github");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+function getTenants(callback){
+
+var github = new GitHubApi({});
+  github.users.getFollowingForUser({
+      // optional
+      // headers: {
+      //     "cookie": "blahblah"
+      // },
+      username: "defunkt"
+  }, function(err, res) {
+      console.log(JSON.stringify(res));
+  });
+}
+
+/* GET tenants */
+router.get('/tenants', function(req, res) {
+  getTenants((tenants) => {
+    res.json(tenants);
+  });
 });
 
 module.exports = router;
