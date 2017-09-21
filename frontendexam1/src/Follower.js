3cbaf4bdf395d2c28bdaf4ae0b178f2f78e5357b
estebandalelr <estebandalelr@gmail.com>
2017-09-21 09:20:00 -0500

    added commentsand some code, does not work tho

--- frontendexam1/src/Follower.js
+++ frontendexam1/src/Follower.js
@@ -6,14 +6,14 @@ class Follower extends Component {
   }
 
 
-//muestra cada uno de los atributos del usuario
+//muestra cada uno de los atributos del usuario con los props
   render() {
     return (
       <div>
         <img className="aptNumber">{this.props.follower.profilepic}</img>
-        <td className="tenantName">{this.props.follower.tenants[0].name}</td>
-        <td className="tenantPhone">{this.props.follower.tenants[0].cel}</td>
-        <td className="tenantPhone">{this.props.follower.tenants[0].email}</td>
+        <p className="tenantName">{this.props.follower.name}</p>
+        <p className="tenantPhone">{this.props.follower.followernumber}</p>
+
       </div>
     );
   }
