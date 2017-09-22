b14efd161df6dc4556571f43cee823975a40d2e8
estebandalelr <estebandalelr@gmail.com>
2017-09-21 22:38:57 -0500

    start logging

--- frontendexam1/src/Followers.js
+++ frontendexam1/src/Followers.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import Follower from './Follower.js';
 
 class Followers extends Component {
+  //Constructor de la clase
   constructor(props) {
     super (props);
   }
 
+  //mapea cada uno de los users
   renderFollowers(){
+    console.log(this.props);
     return this.props.follower.map(
       (t,i)=>{
         return <Follower follower={t} key={i}/>;
       }
     );
   }
-  //mapea cada uno de los users
 
-//hace render de cada map del user
+  //hace render de cada map del user
   render() {
     return (
       <div>
