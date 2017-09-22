82e3757028a33719a5cabf0fe790a25ef031bded
estebandalelr <estebandalelr@gmail.com>
2017-09-22 09:18:35 -0500

    added the orgs

--- frontendexam1/src/Follower.js
+++ frontendexam1/src/Follower.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react';
 class Follower extends Component {
   constructor(props) {
     super (props);
+    // this.state={
+//   orgs: []
+// }
   }
 
-
+  //funcion auxiliar que llama el json de las orgs a las que pertenece el usuario
+  //   componentWillMount(){
+  //     var orgs = this.props.follower.organizations_url;
+  //     //fetch inicial con mi perfil
+  //     fetch(orgs, {method:"GET", headers:{accept: "application/json"}})
+  //     //luego lo revisa y lo convierte a json
+  //     .then((res)=>{if(res.ok) return res.json();})
+  //     //pone en el state los followers, del array data
+  //     .then((orgs)=>{this.setState({orgs: orgs.data});
+  //   });
+  // }
+  //mapea las organizaciones a las que pertenece el user
+  // mapOrgs(){
+  //   return follower.map((t,i)=>
+  //   {return <Follower follower={t} key={i}/>;}
+  // );
 //muestra cada uno de los atributos del usuario con los props
   render() {
     return (
