f3f1447eb7790abba25a5d0756747404ac5d767d
estebandalelr <estebandalelr@gmail.com>
2017-09-22 08:35:04 -0500

    Added post route

--- backendExam1/routes/index.js
+++ backendExam1/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var GitHubApi = require("github");
+var mongodb =require('mongodb');
+
+//hosteado en mi servidor propio, el demonio de mongo
+var url ="mongodb://165.227.187.208:4536/followers";
 
 
-
-/* GET tenants */
+// GET JSON de followers
 router.get('/getfollowers/:username', function(req, res) {
   var github = new GitHubApi({});
   github.users.getFollowingForUser(
@@ -13,4 +16,26 @@ router.get('/getfollowers/:username', function(req, res) {
     });
   });
 
+  //funcion en variable auxiliar al post
+  var insertOrg = function(db, callback) {
+    db.collection('restaurants').insertOne( {
+
+    }, function(err, result) {
+      assert.equal(err, null);
+      console.log("Ahora hay otra organizacion en la bd");
+      callback();
+    });
+  };
+
+  // POST de las organizaciones de los perfiles buscados
+  router.post('/orgs', function(req, res) {
+    var db=req.params.db
+    mongodb.connect(url, function(err, db) {
+      assert.equal(null, err);
+      insertDocument(db, function() {
+        db.close();
+      });
+    });
+  });
+  
   module.exports = router;
